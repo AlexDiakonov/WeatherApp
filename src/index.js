3021999dc909dbfd7c154faa150c181cb2ce5f50
AlexDiakonov <imalex.diakonov@gmail.com>
2020-10-26 11:53:58 +0200

    Mobile version

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './css/normolize.css';
+import './css/mainPage.css';
+import './css/sixDaysPage.css';
+import './api/weather';
+import './js/requests';
+import './js/speech';
+import './js/work2';
+import './css/animations.css';
